c16c78a8c07668244df428a3263cc4460df1cae3
ecelisa <82040168+ecelisa@users.noreply.github.com>
2021-07-02 17:50:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('hola mundo Node!');
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) =>  {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hola mundo Node!');
+});
+
+server.listen(port, () => {
+  console.log('Server running on port: ${port}');
+});
